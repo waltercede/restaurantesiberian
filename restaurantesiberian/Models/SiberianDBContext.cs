a0da6b58b72be8399f1abdde46c64fdfd0c187a2
WALTER CEDEÑO <47424853+waltercede@users.noreply.github.com>
2021-10-18 09:50:19 -0500

    Update SiberianDBContext.cs

--- restaurantesiberian/Models/SiberianDBContext.cs
+++ restaurantesiberian/Models/SiberianDBContext.cs
@@ -24,8 +24,8 @@ namespace restaurantesiberian.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-              //  optionsBuilder.UseSqlServer("Server=190.154.147.130;Database=SiberianDB;User=GEOSHOP;Password=GEOSHOP20;");
-                optionsBuilder.UseSqlServer("Server=190.154.147.130;Database=SiberianDB;User=BIROBID;Password=IROBID20;");
+             
+                optionsBuilder.UseSqlServer("Server=miip;Database=SiberianDB;User=;Password=;");
             }
         }
 
